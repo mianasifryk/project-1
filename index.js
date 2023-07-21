7a2a9feb0f56bf69e57d160002b402c9eea8553a
mianasifryk <140137546+mianasifryk@users.noreply.github.com>
2023-07-20 22:06:42 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+let age = 30;
+const name = "asif";
+let isstudent = true;
+console.log(age);
+console.log(name);
+console.log(isstudent);
---
+++ index.ts
@@ -0,0 +1,8 @@
+let age:number=30;
+const name:string="asif"
+let isstudent:boolean=true;
+
+console.log(age);
+console.log(name);
+console.log(isstudent);
+    
